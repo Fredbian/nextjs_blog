d42bc5fe12c21e4e7294ace9a4520a8e48cd2884
Tao Bian <biantao0519@hotmail.com>
2024-02-26 19:18:31 +1100

    working on page styles

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import styles from './featured.module.css' 
+import React from 'react';
+import styles from './featured.module.css';
+import Image from 'next/image';
 
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Fred here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src={'/p1.jpeg'} className={styles.image} alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Title</h1>
+          <p className={styles.postDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quos
+            optio molestias ipsa, quaerat cumque repudiandae natus harum. Esse
+            quis quibusdam, neque excepturi doloribus ipsam dicta sint adipisci
+            laboriosam dolore!
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
