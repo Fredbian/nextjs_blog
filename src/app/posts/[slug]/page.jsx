119949a81f3f1c6e78a72aee46bbf9324f23a1ed
Tao Bian <biantao0519@hotmail.com>
2024-03-01 09:17:26 +1100

    working on APIs, fixing GET - posts

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -3,29 +3,52 @@ import styles from './post.module.css';
 import Image from 'next/image';
 import Comments from '@/components/comments/Comments';
 
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/post/${slug}`, {
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed');
+  }
+
+  return res.json();
+};
+
 const SinglePage = async ({ params }) => {
+  const { slug } = params;
+
+  const data = await getData(slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>Title</h1>
+          <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image src={'/p1.jpeg'} alt="" fill className={styles.avatar} />
-            </div>
+            {data?.user?.image && (
+              <div className={styles.userImageContainer}>
+                <Image src={data?.user?.image} alt="" fill className={styles.avatar} />
+              </div>
+            )}
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>Username</span>
+              <span className={styles.username}>{data?.user.name}</span>
               <span className={styles.date}>01.01.2024</span>
             </div>
           </div>
         </div>
-        <div className={styles.imageContainer}>
-          <Image src={''} alt="" fill className={styles.image} />
-        </div>
+        {data?.img && (
+          <div className={styles.imageContainer}>
+            <Image src={data?.img} alt="" fill className={styles.image} />
+          </div>
+        )}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div className={styles.description} />
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={{ __html: data?.description }}
+          />
           <div className={styles.comment}>
             <Comments />
           </div>
