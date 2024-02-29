bd6202fd8e68af4714dfe36498f522883bb15dd5
Tao Bian <biantao0519@hotmail.com>
2024-02-29 16:05:31 +1100

    working on Blog page

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -6,19 +6,23 @@ export const GET = async (req) => {
   const { searchParams } = new URL(req.url)
 
   const page = searchParams.get('page')
+  const cat = searchParams.get('cat')
 
   const POST_PER_PAGE = 2
 
   const query = {
     take: POST_PER_PAGE,
     skip: POST_PER_PAGE * (page - 1),
+    where: {
+      ...(cat && { catSlug: cat }),
+    }
   }
 
   try {
     // '$transaction' in Prisma can make multiple requests in one query
     const [posts, count] = await prisma.post.$transaction([
       prisma.post.findMany(query),
-      prisma.post.count()
+      prisma.post.count({where: query.where})
     ])
 
     return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }))
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,27 +3,30 @@ import Image from 'next/image';
 import styles from './card.module.css';
 import Link from 'next/link';
 
-const Card = ({key, item}) => {
+const Card = ({ key, item }) => {
   return (
     <div className={styles.container} key={key}>
-      <div className={styles.imageContainer}>
-        <Image src={'/p1.jpeg'} alt="" fill className={styles.image} />
-      </div>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.02.2023 - </span>
-          <span className={styles.category}>CULTURE</span>
+          <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{' '}
+          </span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href={'/'}>
+        <Link href={`/posts/${item.slug}`}>
           <h1>{item?.title}</h1>
         </Link>
         <p className={styles.description}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis omnis
-          voluptas ipsam facere maiores iste aliquam ad repellendus sunt,
-          tempore quasi, ratione maxime dicta pariatur. Sequi expedita aliquid
-          natus totam.
+          {item.description.substring(0, 60)}
         </p>
-        <Link href={'/'} className={styles.link}>Read More</Link>
+        <Link href={`/posts/${item.slug}`} className={styles.link}>
+          Read More
+        </Link>
       </div>
     </div>
   );
