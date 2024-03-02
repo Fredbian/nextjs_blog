1842c60ee41eb05c60886e21334276e88cb9bf26
Tao Bian <biantao0519@hotmail.com>
2024-03-02 12:11:20 +1100

    Feature-Add new Post

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE,
+  authDomain: "fun-blog-58baf.firebaseapp.com",
+  projectId: "fun-blog-58baf",
+  storageBucket: "fun-blog-58baf.appspot.com",
+  messagingSenderId: "331286096720",
+  appId: "1:331286096720:web:488bc9fd933cbcf5469a55"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
